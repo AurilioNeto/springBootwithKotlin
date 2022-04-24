2ab18361f7708a615b6d7af8dc8bd95b5a71d471
Aurilio Neto <aurilioneto30@gmail.com>
2022-04-24 17:38:53 +0100

    Web Service

--- src/main/kotlin/com/example/netoprojects/springbootwithkotlin/HelloWorldController.kt
+++ src/main/kotlin/com/example/netoprojects/springbootwithkotlin/HelloWorldController.kt
@@ -1,9 +1,13 @@
 package com.example.netoprojects.springbootwithkotlin
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/hello")
 class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String = "Hello, this is a REST endpoint!"
 }
