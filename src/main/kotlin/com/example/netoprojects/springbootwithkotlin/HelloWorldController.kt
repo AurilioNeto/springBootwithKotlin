9bafaecf7b5b1fa8fc1111d03d9aab4ffa2e2091
Aurilio Neto <aurilioneto30@gmail.com>
2022-04-24 17:29:46 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/netoprojects/springbootwithkotlin/HelloWorldController.kt
@@ -0,0 +1,9 @@
+package com.example.netoprojects.springbootwithkotlin
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+}
---
+++ src/main/kotlin/com/example/netoprojects/springbootwithkotlin/SpringBootWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.netoprojects.springbootwithkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootWithKotlinApplication>(*args)
+}
